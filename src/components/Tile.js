8b6734dbdcd1f6820f2f4bd9293891cc334eda2b
Jelena Spocova <Jelena.Spocova@nrk.no>
2019-11-14 22:49:53 +0100

    flags

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -2,6 +2,26 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ReactCountryFlag from "react-country-flag";
 import {Link} from 'react-router-dom';
+import ReactSVG from 'react-svg'
+
+function renderFlag(nationality) {
+    switch(nationality) {
+        case "no": 
+        return <ReactSVG src=".././resources/250px-Flag_of_Norway.svg.webp"/>
+        case "dk": 
+        return <ReactSVG src=".././resources/250px-Flag_of_Denmark.svg.webp"/>
+        case "se": 
+        return <ReactSVG src=".././resources/250px-Flag_of_Sweden.svg.webp"/>
+        case "fi": 
+        return <ReactSVG src=".././resources/250px-Flag_of_Finland.svg.webp"/>
+        case "is": 
+        return <ReactSVG src=".././resources/250px-Flag_of_Island.svg.webp"/>
+        default:
+        return
+        break;
+        
+    }
+}
 
 const Tile = ({ id, title, logo, imgSrc, flagSrc, hearts, nationality }) => {
     return (
@@ -10,7 +30,7 @@ const Tile = ({ id, title, logo, imgSrc, flagSrc, hearts, nationality }) => {
                 <img src={imgSrc} className="food-image"/>
                 <img src={logo} className="logo" />
                 <div className="flag">
-                    <ReactCountryFlag code={nationality} />
+                    {renderFlag(nationality) }
                 </div>
                 <img src={logo} className="logo"/>
                 <h2 className="tile-title">{title}</h2>

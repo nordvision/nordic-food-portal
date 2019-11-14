c2ec2b5ec3ab2dd44d9e28cf00e2dd2699b4b343
Ingrid Grønlie Guren <ingrid.gronlie.guren@nrk.no>
2019-11-14 22:46:37 +0000

    Styling of search

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -5,7 +5,7 @@ import {Link} from 'react-router-dom';
 const Menu = () => {
     return (
         <div className="menu">
-            <Link to="/about" className="menu-item">Nordic Food</Link>
+            <Link to="/" className="menu-item">Nordic Food</Link>
             <Link to="/seasons" className="menu-item">Seasons</Link>
             <Link to="/stories" className="menu-item">Food stories</Link>
             <Link to="/search" className="menu-item">Search</Link>

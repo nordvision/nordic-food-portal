0e9040fda619c15e8d20d622ff80001c8d240cb5
Jelena Spocova <Jelena.Spocova@nrk.no>
2019-11-14 22:28:07 +0100

    home tile wrapping fixed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,47 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import video from '../resources/maaemo-film.mp4'
-import {Menu} from '../components/Menu';
+import { Menu } from '../components/Menu';
+import SearchResult from '../components/SearchResult';
 import autumnImage from './../resources/autumn.png';
 
-function Home() {
-  return (
-    <div className="Home page-background">
-        <div className="home-background-wrapper">
-        <div className="home-title-wrapper">
-            <h1 className="home-title">Nordic food portal </h1>
-            <p>Recipes, food culture and stories from the Nordic public broadcasters</p>
-        </div>
-        <video autoPlay loop className="home-video">
-            <source src={video} type="video/mp4" />
-            <Menu />
-        </video>
-        </div>
-        <div className="title-explore">
-            <p>Explore seasonal</p>
-            <p>food from the</p>
-            <p>Northern contries</p>
-        </div>
+class Home extends Component {
+    render() {
+        return (
+            <div className="Home page-background">
+                <div className="home-background-wrapper">
+                    <div className="home-title-wrapper">
+                        <h1 className="home-title">Nordic food portal </h1>
+                        <p>Recipes, food culture and stories from the Nordic public broadcasters</p>
+                    </div>
+                    <video autoPlay loop className="home-video">
+                        <source src={video} type="video/mp4" />
+                        <Menu />
+                    </video>
+                </div>
+                <div className="title-explore">
+                    <p>Explore seasonal</p>
+                    <p>food from the</p>
+                    <p>Northern contries</p>
+                </div>
 
-        <div className="autumn">
-            <div className="w-50">
-                <h2 className="season-title">Autumn</h2>
-                <p>Autumn is the time for harvest, hunting and conservation. Explore recepies from the Scaninvian countries.</p>
-                <p className="underlined-text">Show Autumn recepies</p>
-                <p className="underlined-text">Read more about autumn in scandinavia</p>
-            </div>
-            <div className="w-50">
-                <img src={autumnImage} />
-            </div>
-        </div>
+                <div className="autumn">
+                    <div className="w-50">
+                        <h2 className="season-title">Autumn</h2>
+                        <p>Autumn is the time for harvest, hunting and conservation. Explore recepies from the Scaninvian countries.</p>
+                        <p className="underlined-text">Show Autumn recepies</p>
+                        <p className="underlined-text">Read more about autumn in scandinavia</p>
+                    </div>
+                    <div className="w-50">
+                        <img src={autumnImage} />
+                    </div>
+                </div>
 
-        <div className="autumn-recipes">
-            <div>
-                <h2 className="tiles-title">Autumn recipes</h2>
+                <div className="autumn-recipes">
+                    <div>
+                        <h2 className="tiles-title">Autumn recipes</h2>
+                    </div>
+                    <SearchResult query="salmon" numberOfResults={4} />
+                </div>
             </div>
-        </div>
-    </div>
-  );
+        );
+    }
 }
 
 Home.propTypes = {};

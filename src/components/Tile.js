5d65b54733ae1cb63e9b088a6ce321a05c3aac2f
Ingrid Grønlie Guren <ingrid.gronlie.guren@nrk.no>
2019-11-14 22:38:40 +0000

    Add styling and flags

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -3,38 +3,28 @@ import PropTypes from 'prop-types';
 import ReactCountryFlag from "react-country-flag";
 import {Link} from 'react-router-dom';
 import ReactSVG from 'react-svg'
+import { NrkHeartActive } from '@nrk/core-icons/jsx'  // React, ReactElement
+import Flag from './Flag';
 
-function renderFlag(nationality) {
-    switch(nationality) {
-        case "no": 
-        return <ReactSVG src=".././resources/250px-Flag_of_Norway.svg.webp"/>
-        case "dk": 
-        return <ReactSVG src=".././resources/250px-Flag_of_Denmark.svg.webp"/>
-        case "se": 
-        return <ReactSVG src=".././resources/250px-Flag_of_Sweden.svg.webp"/>
-        case "fi": 
-        return <ReactSVG src=".././resources/250px-Flag_of_Finland.svg.webp"/>
-        case "is": 
-        return <ReactSVG src=".././resources/250px-Flag_of_Island.svg.webp"/>
-        default:
-        return
-        break;
-        
-    }
-}
 
-const Tile = ({ id, title, logo, imgSrc, flagSrc, hearts, nationality }) => {
+const Tile = ({id, title, logo, imgSrc, flagSrc, hearts, nationality}) => {
     return (
         <Link className="title-wrapper" to={`/recipe/${id}`}>
             <div className="tile">
                 <img src={imgSrc} className="food-image"/>
-                <img src={logo} className="logo" />
-                <div className="flag">
-                    {renderFlag(nationality) }
+                <div className="tile-information">
+                    <div className="flag">
+                        <Flag nationality={nationality} />
+                    </div>
+                    <h2 className="tile-title">{title}</h2>
                 </div>
-                <img src={logo} className="logo"/>
-                <h2 className="tile-title">{title}</h2>
-                <span>{hearts}</span>
+                <div className="logos">
+                    <img src={logo} className="logo"/>
+                    <div className="likes">
+                        <NrkHeartActive />
+                        <span>{hearts}</span>
+                    </div>
+                </div>
             </div>
         </Link>
     );
